f3bdce18ead715168e1d73b83814f7fb8ecfa56c
Tyler <tylertruong124@gmail.com>
2026-03-30 13:58:15 -0700

    Changed db creation to start with Gunicorn

--- backend/main.py
+++ backend/main.py
@@ -22,9 +22,8 @@ def paste(token):
     entry = ClipboardEntry.query.filter_by(token=token).first_or_404()
     return jsonify({"content": entry.content})
 
+with app.app_context():
+    db.create_all()
 
 if __name__ == "__main__":
-    with app.app_context():
-        db.create_all()
-
     app.run(debug=True)
